0fa063fde3dae3e82f09a8f301d4cb33670cc8c2
Arnaud Lemarquand <arnaud.lemarquand@gmail.com>
2016-11-23 15:11:45 +0100

    Ajout projet pizzeria-console-objet

--- pizzeria-console-objet/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-objet/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -2,20 +2,26 @@ package fr.pizzeria.console;
 
 import java.util.Scanner;
 
+import fr.pizzeria.model.Pizza;
+
 public class PizzeriaAdminConsoleApp {
 	
 	
 	
 	public static void main(String args[]){
-				
-		String pizzas[][]  = {{"0","PEP","Pépéroni","12.50"},
-							  {"1","MAR","Margherita","14.00"},
-							  {"2","REI","La Reine","11.50"},
-							  {"3","FRO","La 4 fromages","12.00"},
-							  {"4","CAN","La cannibale","12.50"},
-							  {"5","SAV","La savoyarde","13.00"},
-							  {"6","ORI","L'orientale","13.50"},
-							  {"7","IND","L'indienne","14.00"}};
+	
+		Pizza[] pizzas;
+		pizzas= new Pizza[8];
+		pizzas[0] = new Pizza(0, "PEP", "Pépéroni", 12.50);
+		pizzas[1] = new Pizza(1, "MAR", "Margherita", 14.00);
+		pizzas[2] = new Pizza(2, "REI", "La Reine", 11.50);
+		pizzas[3] = new Pizza(3, "FRO", "La 4 fromages", 11.50);
+		pizzas[4] = new Pizza(4, "CAN", "La cannibale", 11.50);
+		pizzas[5] = new Pizza(5, "SAV", "La savoyarde", 11.50);
+		pizzas[6] = new Pizza(6, "ORI", "L'orientale", 11.50);
+		pizzas[7] = new Pizza(7, "IND", "L'indienne", 11.50);
+		Pizza.nbPizzas = 8;
+
 		
 		Scanner sc = new Scanner(System.in);
 		String choix;
@@ -65,14 +71,14 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("99. Sortir");	
 	}
 	
-	public static void listPizzas(String[][] pizzas){
+	public static void listPizzas(Pizza[] pizzas){
 		//Listing de toutes les pizzas
-		for(int i=0; i<pizzas.length; i++) {
-			System.out.println(pizzas[i][1] + " -> " + pizzas[i][2] +  " (" + pizzas[i][3] + "€)");
+		for(int i=0; i<Pizza.nbPizzas; i++) {
+			System.out.println(pizzas[i].code + " -> " + pizzas[i].nom +  " (" + pizzas[i].prix + "€)");
 		}
 	}
 	
-	public static String[][] ajouterPizza(Scanner sc, String[][] pizzas){
+	public static Pizza[] ajouterPizza(Scanner sc, Pizza[] pizzas){
 		
 		//Enregistrement des valeurs de l'utilisateur
 		System.out.println("Veuillez saisir le code");
@@ -82,28 +88,26 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("Veuillez saisir le prix");
 		String prix = sc.nextLine();
 		
+	
 		//Création d'un nouveau tableau temporaire et ajout des anciennes valeurs de pizzas
-		String[][] newPizzas = new String[(pizzas.length)+1][4];
-		for(int i=0; i<pizzas.length; i++) {
+		Pizza[] newPizzas;
+		newPizzas = new Pizza[Pizza.nbPizzas+1];
+		for(int i=0; i<Pizza.nbPizzas; i++) {
 			newPizzas[i] = pizzas[i];
 		}
 		
-		//index de la nouvelle pizza à ajouter
-		int posPizza = pizzas.length;
+		//Ajout de la nouvelle pizza
+		newPizzas[Pizza.nbPizzas] = new Pizza(Pizza.nbPizzas+1, code, nom, Double.parseDouble(prix));
 		
-		//ajout de la nouvelle pizza
-		newPizzas[posPizza][0] = String.valueOf((newPizzas.length));
-		newPizzas[posPizza][1] = code;
-		newPizzas[posPizza][2] = nom;
-		newPizzas[posPizza][3] = prix;
-	
+		//Increment du nombre de pizza
+		Pizza.nbPizzas++;	
 		
 		System.out.println("Pizza ajoutée");
 		return newPizzas;
 		
 	}
 	
-	public static String[][] majPizza(Scanner sc, String[][] pizzas) {
+	public static Pizza[] majPizza(Scanner sc, Pizza[] pizzas) {
 		
 		System.out.println("Mise à jour d'une pizza");
 		//Listing de toute les pizzas
@@ -112,25 +116,27 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("Veuillez choisir la pizza à modifier");
 		System.out.println("99 pour abandonner");
 		
+		//Recherche de la pizza à modifier
 		String codePizza = sc.nextLine();
 		int i=0;
-		while(codePizza.equals(pizzas[i][1]) == false)
+		while(codePizza.equals(pizzas[i].code) == false)
 		{
 			i++;
 		}
 		
+		//Demande des saisies avec enregistrement des modifications
 		System.out.println("Veuillez saisir le code");
-		pizzas[i][1] = sc.nextLine();
+		pizzas[i].code = sc.nextLine();
 		System.out.println("Veuillez saisir le nom (sans espace)");
-		pizzas[i][2] = sc.nextLine();
+		pizzas[i].nom = sc.nextLine();
 		System.out.println("Veuillez saisir le prix");
-		pizzas[i][3] = sc.nextLine();
+		pizzas[i].prix = Double.parseDouble(sc.nextLine());
 		
 		System.out.println("Liste des pizzas modifiée");
 		return pizzas;
 	}
 	
-	public static String[][] supprimerPizza(Scanner sc, String[][] pizzas) {
+	public static Pizza[] supprimerPizza(Scanner sc, Pizza[] pizzas) {
 	
 		System.out.println("Mise à jour d'une pizza");
 		//Listing de toute les pizzas
@@ -146,20 +152,23 @@ public class PizzeriaAdminConsoleApp {
 		
 		//index de la pizza à supprimer
 		int i=0;
-		while(codePizza.equals(pizzas[i][1]) == false)
+		while(codePizza.equals(pizzas[i].code) == false)
 		{
 			i++;
 		}	
 		
 		//Création d'un nouveau tableau temporaire avec suppression de la pizza voulue
-		String[][] newPizzas = new String[(pizzas.length)-1][4];
+		Pizza[] newPizzas = new Pizza[(Pizza.nbPizzas)-1];
 		int indexNewPizza = 0;
-		for(int j=0; j<pizzas.length; j++) {
+		for(int j=0; j<Pizza.nbPizzas; j++) {
 			if(j!=i){
 				newPizzas[indexNewPizza] = pizzas[j];
 				indexNewPizza++;
 			}
 		}
+		Pizza.nbPizzas--;
 		return newPizzas;
 	}
+	
+	
 }
