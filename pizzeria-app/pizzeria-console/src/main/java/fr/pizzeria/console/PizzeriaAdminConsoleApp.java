837b1358bac3b195960b2f05a38714883e7bb7ec
Arnaud Lemarquand <arnaud.lemarquand@gmail.com>
2016-12-05 16:01:35 +0100

    ajout de application.properties

--- pizzeria-app/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-app/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,33 +1,22 @@
 package fr.pizzeria.console;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import fr.pizzeria.abstractfactory.AbstractFactory;
-import fr.pizzeria.abstractfactory.FichierFactory;
-import fr.pizzeria.abstractfactory.TableauFactory;
 import fr.pizzeria.dao.PizzaDao;
 import fr.pizzeria.ihm.IhmUtil;
 import fr.pizzeria.ihm.Menu;
-import fr.pizzeria.model.CategoriePizza;
-import fr.pizzeria.model.Pizza;
-
 
 public class PizzeriaAdminConsoleApp {
 
-	public static void main(String[] args) {
-		Pizza p1 = new Pizza("PEP", "Pépéroni", 12.50, CategoriePizza.VIANDE);
-      
-        
-        Pizza p2 = new Pizza("PEP", "Pépéroni", 12.50, CategoriePizza.VIANDE);
-        
-        System.out.println("p1=p2 ? " + p1.equals(p2));
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		ResourceBundle bundle = ResourceBundle.getBundle("application");
+		String daoConfig = bundle.getString("dao.impl");
+		AbstractFactory daoFactory = (AbstractFactory) Class.forName(daoConfig).newInstance();
 		
-		
-		
-		 AbstractFactory factory = new TableauFactory();
-		//AbstractFactory factory = new FichierFactory();
 
-		PizzaDao pizzaDao = factory.getService().getPizzas();
+		PizzaDao pizzaDao = daoFactory.getService().getPizzas();
 		Scanner sc = new Scanner(System.in);
 		IhmUtil ihmUtil = new IhmUtil(sc, pizzaDao);
 
